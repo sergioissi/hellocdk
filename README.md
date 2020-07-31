630eff4218ce253abbabf47e80fe5d204825c611
Sergio Issi <sergio.issi@gmail.com>
2020-07-31 17:34:07 +0200

    :octocat: test

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# hellocdk
+# Hello World
+
+This is an example project
+
+
+## Installation
+
+```python
+pip install hellocdk
+```
+
+## Usage
+
+```python
+from hellocdk import say_hello
+
+# Generate "Hello, World!"
+say_hello()
+
+# Generate "Hello, Everybody!"
+say_hello("Everybody")
+```
+
+
---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+setup(
+    name='hellocdk',
+    version='0.0.1',
+
+    descritption="A test CDK Python app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    py_modules=["hellocdk"],
+    package_dir={'': 'src'},
+
+    install_requires=[
+        "aws-cdk.core==1.55.0",
+    ],
+
+    python_requires=">=3.6",
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+
+        "Intended Audience :: Developers",
+
+        "License :: OSI Approved :: GNU General Public License v3.0",
+
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Utilities",
+
+        "Typing :: Typed",
+    ],
+)
